f114fca4c61387e02cef2e0cf06810537db26a47
Антон Красноперов <an2niy85@mail.ru>
2021-11-17 23:25:26 +0400

    Добавьте файлы проекта.

---
+++ Repeat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase;
+            int countRepetitions;
+            int i;
+            
+            Console.WriteLine("Введите фразу?");
+            phrase = Console.ReadLine();
+            Console.WriteLine("Введите кол-во повторов?");
+            countRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < countRepetitions; i++)
+            {
+                Console.WriteLine(phrase);
+            }
+            Console.ReadLine();
+        }
+    }
+}
