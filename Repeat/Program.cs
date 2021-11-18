4b2aca3dbdaca1bab0d97126c2a41464c821c7ed
an2niy85 <an2niy85@mail.ru>
2021-11-18 10:49:18 +0400

    the defination of the counter variable was moved inside the loop

--- Repeat/Program.cs
+++ Repeat/Program.cs
@@ -12,14 +12,14 @@ namespace Repeat
         {
             string phrase;
             int countRepetitions;
-            int i;
+           
             
             Console.WriteLine("Введите фразу?");
             phrase = Console.ReadLine();
             Console.WriteLine("Введите кол-во повторов?");
             countRepetitions = Convert.ToInt32(Console.ReadLine());
 
-            for (i = 0; i < countRepetitions; i++)
+            for (int i = 0; i < countRepetitions; i++)
             {
                 Console.WriteLine(phrase);
             }
